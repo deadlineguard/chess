fbb7759fe5445f384a05d535dd278c63bca65cf1
valeriyshashkin <velianjanjan@gmail.com>
2021-01-07 21:58:31 +0500

    add not found page

--- app/frontend/src/NotFound.js
+++ app/frontend/src/NotFound.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./NotFound.scss"
 
 const NotFound = () => (
-    <div>
+    <div className="centered">
         <h1>404</h1>
-        <p>Страница не найдена</p>
+        <p className="desc">Страница не найдена</p>
     </div>
 );
 
