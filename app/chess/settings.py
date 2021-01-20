967dea13037edac8c938f8e5166caf9d487aeda5
ValeriyShashkin <shashkin_val@mail.ru>
2021-01-20 12:38:07 +0500

    fix

--- app/chess/settings.py
+++ app/chess/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static/'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
